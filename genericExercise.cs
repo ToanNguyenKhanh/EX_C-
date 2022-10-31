080446d62be62be4c79d4737b54d3d2736ef476a
TKN <111730598+ToanNguyenKhanh@users.noreply.github.com>
2022-10-31 20:18:09 +0700

    Add files via upload

---
+++ genericExercise.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Programming
+{
+    internal class Program
+    {
+        static object sum<T>(object var1, object var2)
+        {
+            T sum;
+            sum = (dynamic)var1 + (dynamic)var2;
+            return sum;
+        }
+        static void sumarray<T>(T[] arr1, T[] arr2, out T[] arr3)
+        {
+            arr3 = new T[5];
+            for (int i = 0; i < 5; i++)
+            {
+                arr3[i] = (dynamic)arr1[i] + arr2[i];
+            }            
+        }
+        static void showMatrix(int[] k)
+        {            
+            for (int i = 0; i <k.Length; i++)
+            {
+                Console.Write(k[i] + " ");
+            }
+        }    
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EX 1:");
+            Console.OutputEncoding = Encoding.UTF8;
+            int a = 5, b = 6;
+            float c = 13.5f, d = 34.6f;
+            string e = "Hello", f = " World";
+            int[] g = { 1, 2, 3, 4, 5 };
+            int[] h = { 6, 7, 8, 9, 10 };            
+            int[] k = new int[5];             
+            Console.WriteLine("{0} + {1} = {2}", a, b, sum<int>(a, b));
+            Console.WriteLine("{0} + {1} = {2}", c, d, sum<float>(c, d));
+            Console.WriteLine("{0} + {1} = {2}", e, f, sum<string>(e, f));
+            sumarray<int>(g,h,out k);
+            Console.Write("Mảng g: "); showMatrix(g);
+            Console.Write("Mảng h: "); showMatrix(h);
+            Console.Write("Mảng k = g + h = "); showMatrix(k);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ timingExercise.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApplication2
+{
+    public class Timing
+    {
+        TimeSpan startingTime;
+        TimeSpan duration;
+        public Timing()
+        {
+            startingTime = new TimeSpan(0);
+            duration = new TimeSpan(0);
+        }
+        public void StopTime()
+        {
+            duration =
+            Process.GetCurrentProcess().Threads[0].
+            UserProcessorTime.
+            Subtract(startingTime);
+        }
+        public void startTime()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            startingTime =
+            Process.GetCurrentProcess().Threads[0].
+            UserProcessorTime;
+        }
+        public TimeSpan Result()
+        {
+            return duration;
+        }
+    }
+    public class Program
+    {
+        static void Array(int[] arr)
+        {
+            Random r = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = r.Next(1, 9999);
+            }
+        }
+        static void maxMinArray(int[] arr)
+        {
+            int max = arr[0]; int min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                else if(arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            Console.Write(max+"\t");
+            Console.WriteLine(min);
+        }     
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int ran = 10000000;
+            int[] arr = new int[ran];
+            Array(arr);
+            Timing time = new Timing();
+            time.startTime();
+            Console.Write("Phần tử lớn nhất và nhỏ nhất lần lượt là: "); 
+            maxMinArray(arr);
+            time.StopTime();
+            Console.Write("Timing: "); Console.WriteLine(time.Result().TotalSeconds);
+            Console.ReadLine();
+        }
+    }
+}
